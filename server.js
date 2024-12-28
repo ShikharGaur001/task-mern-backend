d19f795d526c1ac3cfd62e2b03e1dfd89adb7008
Shikhar Gaur <shikhargaur2008@gmail.com>
2024-12-28 15:18:33 +0530

    Cors Added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { errorHandler } = require("./middlewares/error.middleware");
 const connectDB = require("./connections/database");
+const cors = require('cors')
 
 connectDB();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use("/api/tasks", require("./routes/task.routes"));
 app.use("/api/users", require("./routes/user.routes"));
